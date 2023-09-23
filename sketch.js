1eaafc0bfe1e8e5a082a1848dfc58f4c861c0efe
Simra Khan <simrakhan7605@gmail.com>
2023-09-23 19:55:03 -0400

    full functionality of main menu and instructions

--- sketch.js
+++ sketch.js
@@ -1,33 +1,74 @@
-let bg;
-let edgeX = 1100;
-function setup() {
-  font = loadFont('fonts/fibberish.ttf');
-  textFont(font);
-  textSize(150);
+let gameState = 0;
+let titleScreenBackground;
 
-  bg = loadImage("assets/background.png");
+function setup() {
   createCanvas(1000, 600);
+
+  noStroke();
+
+  titleFont = loadFont('fonts/fibberish.ttf');
+  titleScreenBackground = loadImage("assets/background.png");
 
   playButton = createButton("Play");
   helpButton = createButton("Instructions");
+  backToMenuButton = createButton("Back to Title Screen");
+}
+
+function draw() {
+  checkGameState();
+}
+
+function checkGameState() {
+  if (gameState == 0) {
+    titleScreen();
+  } else if (gameState == 1) {
+    partAIInstructionsMenu(0);
+  }
+}
+
+function titleScreen() {
+  background(titleScreenBackground);
+
+  gameState = 0;
+
+  textFont(titleFont);
+
+  playButton.show();
+  helpButton.show();
+  backToMenuButton.hide();
 
   playButton.position(400, 250);
   helpButton.position(360, 350);
 
-  playButton.mouseClicked(change);
-  helpButton.mouseClicked(change);
-}
+  playButton.mouseClicked(partAInstructionsMenu);
+  helpButton.mouseClicked(partAInstructionsMenu);
 
-function draw() {
-  background(bg);
+  // title text colour
   fill(255);
-
+  textSize(150);
+  textFont(titleFont);
   text("title", 350, 200);
 
-  fill(0, 0, 0, 100)
-  rect(edgeX, 100, 800, 400);
 }
 
-function change() {
-  edgeX = 0;
+function partAInstructionsMenu() {
+  background(titleScreenBackground);
+
+  gameState = 1;
+
+  playButton.hide();
+  helpButton.hide();
+  backToMenuButton.show();
+
+  backToMenuButton.position(300, 350);
+
+  backToMenuButton.mouseClicked(titleScreen);
+
+  fill(255);
+  textSize(150);
+  text("instructions", 150, 200);
+
+  textSize(30);
+  text("Press the spacebar to move through the text.", 200, 300);
+
 }
